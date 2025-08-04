8ab506fd7013ea6eae5b9ae402c49b633d8bc4da
Amir-Subaih <86726698+Amir-Subaih@users.noreply.github.com>
2025-08-04 13:53:43 +0300

    up

--- src/components/web/Footer/Footer.jsx
+++ src/components/web/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import style from "./footer.module.css";
 
-const Footer = () => {
+function Footer (){
   return (
     <footer className={`${style.footer}`}>
       <div className={`${style.footertop}`}>
