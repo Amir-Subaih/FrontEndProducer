a6215ee44053d0ee051762a2e05fb246e3d4f200
Amir-Subaih <86726698+Amir-Subaih@users.noreply.github.com>
2025-08-04 13:28:30 +0300

    up

--- src/layout/WebLayout.jsx
+++ src/layout/WebLayout.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Navbar from '../components/web/navbar/Navbar.jsx'
-import Footer from '../components/web/footer/Footer.jsx'
+import Navbar from '../components/web/navbar/Navbar'
+import Footer from '../components/web/footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 export default function WebLayout() {
